4abd5f1fac712408fc962840581d0f978cea44a4
Josh <59149060+j-o-sh@users.noreply.github.com>
2020-07-10 12:31:33 +0200

    problem with  query parameters?

--- functions/something.js
+++ functions/something.js
@@ -1,10 +1,13 @@
-module.exports.handler = function handler (_q, _c, callback) {
+module.exports.handler = function handler (request, _c, callback) {
+
   callback(null, {
       statusCode: 200,
       body: `
       <html><body>
       <h1>Nothing to see here</h1>
       <p>Well this sucks... go <a href="/">back?</a></p>
+      <hr>
+      ${JSON.stringify(request, null, 2)}
       </body></html>`
     })
 }
